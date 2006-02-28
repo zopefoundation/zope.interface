100a9b9e2a0c4552cf7c470fb4da15db2bb3a1e2
Nathan Yergler <nathan@yergler.net>
2006-02-28 00:31:24 +0000

    Added setup.py for use when ovulating eggs.

---
+++ README.txt
@@ -0,0 +1,5 @@
+==========
+Interfaces
+==========
+
+See ./src/zope/interface/README.txt for details and example usage.
---
+++ setup.py
@@ -0,0 +1,48 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.interface package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportException, e:
+    from distutils.core import setup, Extension
+    
+setup(name='zope.interface',
+      version='3.0',
+
+      url='http://svn.zope.org/zope.interface',
+      license='ZPL 2.1',
+      description='Zope 3 Interface Infrastructure',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='The implementation of interface definitions for Zope 3.',
+      
+      packages=["zope", "zope.interface"],
+      package_dir = {'': 'src'},
+      ext_package='zope.interface',
+      ext_modules=[Extension("_zope_interface_coptimizations",
+                             [os.path.join('src', 'zope', 'interface',
+                                           "_zope_interface_coptimizations.c")
+                              ]),
+                   ],
+
+      tests_require = ['zope_testing'],
+      include_package_data = True,
+      zip_safe = False,
+      )
