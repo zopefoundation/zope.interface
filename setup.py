06bd0e5afe5954138e5b9ec8fcc09f06979892a5
Tres Seaver <tseaver@palladion.com>
2006-04-15 00:07:32 +0000

    3.2.0.1:  suppress inclusion of setup.cfg in sdist builds.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
     
 setup(name='zope.interface',
-      version='3.2.0',
+      version='3.2.0.1',
 
       url='http://svn.zope.org/zope.interface/tags/3.2.0',
       license='ZPL 2.1',
