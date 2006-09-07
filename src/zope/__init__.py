61cb667dd2b347cf88120d3f7b7cb44138acf642
Fred Drake <fdrake@acm.org>
2006-09-07 15:52:25 +0000

    make happier eggs

--- src/zope/__init__.py
+++ src/zope/__init__.py
@@ -1,1 +1,7 @@
-__import__('pkg_resources').declare_namespace(__name__)
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
