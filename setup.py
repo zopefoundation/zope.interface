9f31a49006cc1ed223f89ee76cf0f0ef44468075
Georgy Berdyshev <codingmaster@gmail.com>
2008-07-08 12:31:51 +0000

    Added optional code optimizations support, which allows the building of C code optimizations to fail.
    Signed-off-by: Georgy Berdyshev - Георгий Бердышев <codingmaster@gmail.com>

--- setup.py
+++ setup.py
@@ -18,8 +18,12 @@ $Id$
 
 import os, sys
 
+from distutils.command.build_ext import build_ext
+from distutils.errors import (CCompilerError, DistutilsExecError, 
+                              DistutilsPlatformError)
+
 try:
-    from setuptools import setup, Extension
+    from setuptools import setup, Extension, Feature
 except ImportError, e:
     from distutils.core import setup, Extension
 
@@ -34,12 +38,23 @@ except ImportError, e:
         extra = {}
 
 else:
+    codeoptimization = Feature("Optional code optimizations",
+                               standard = True,
+                               ext_modules = [Extension(
+                                             "_zope_interface_coptimizations",
+                                             [os.path.normcase(
+                                             os.path.join('src', 'zope',
+                                             'interface',
+                                             '_zope_interface_coptimizations.c')
+                                             )]
+                                             )])
     extra = dict(
         namespace_packages=["zope"],
         include_package_data = True,
         zip_safe = False,
         tests_require = ['zope.testing'],
         install_requires = ['setuptools'],
+        features = {'codeoptimization': codeoptimization}
         )
 
 def read(*rnames):
@@ -63,6 +78,38 @@ long_description=(
         '**********************\n'
         )
 
+
+class optional_build_ext(build_ext):
+    """This class subclasses build_ext and allows
+       the building of C extensions to fail.
+    """
+    def run(self):
+        try:
+            build_ext.run(self)
+        
+        except DistutilsPlatformError, e:
+            self._unavailable(e)
+
+    def build_extension(self, ext):
+       try:
+           build_ext.build_extension(self, ext)
+        
+       except (CCompilerError, DistutilsExecError), e:
+           self._unavailable(e)
+
+    def _unavailable(self, e):
+        print >> sys.stderr, '*' * 80
+        print >> sys.stderr, """WARNING:
+
+        An optional code optimization (C extension) could not be compiled.
+
+        Optimizations for this package will not be available!"""
+        print >> sys.stderr
+        print >> sys.stderr, e
+        print >> sys.stderr, '*' * 80
+    
+
+
 setup(name='zope.interface',
       version = '3.5dev',
       url='http://www.python.org/pypi/zope.interface',
@@ -75,9 +122,5 @@ setup(name='zope.interface',
       packages = ['zope', 'zope.interface'],
       package_dir = {'': 'src'},
       ext_package='zope.interface',
-      ext_modules=[Extension("_zope_interface_coptimizations",
-                             [os.path.join('src', 'zope', 'interface',
-                                           "_zope_interface_coptimizations.c")
-                              ]),
-                   ],
+      cmdclass = {'build_ext': optional_build_ext},
       **extra)
