f59f3bdb55f07cf7111fd6a469eb43f7ede560f0
Tres Seaver <tseaver@palladion.com>
2010-04-06 02:59:44 +0000

    Comply with repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2004-2007 Zope Corporation and Contributors.
+# Copyright (c) 2004-2007 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -124,7 +124,7 @@ setup(name='zope.interface',
       url='http://pypi.python.org/pypi/zope.interface',
       license='ZPL 2.1',
       description='Interfaces for Python',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       long_description=long_description,
 
