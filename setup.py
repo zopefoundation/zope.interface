78e878cdcb4c71bc6ed4b5dc0b200d44b3449feb
Tres Seaver <tseaver@palladion.com>
2011-08-13 17:52:35 +0000

    Prep 3.7.0 release.

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ except (ImportError, SyntaxError):
     from build_ext_2 import optional_build_ext
     
 setup(name='zope.interface',
-      version='3.7.0dev',
+      version='3.7.0',
       url='http://pypi.python.org/pypi/zope.interface',
       license='ZPL 2.1',
       description='Interfaces for Python',
