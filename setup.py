d88751d4cf1a2ee8085082d5a384062165306eb6
Tres Seaver <tseaver@palladion.com>
2016-06-10 10:00:49 -0400

    svb

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ long_description=(
         )
 
 setup(name='zope.interface',
-      version='4.2.0',
+      version='4.2.1.dev0',
       url='https://github.com/zopefoundation/zope.interface',
       license='ZPL 2.1',
       description='Interfaces for Python',
