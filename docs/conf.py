dd50fc30beff9a2da301fb97db4636c95fccef56
Jason Madden <jamadden@gmail.com>
2018-07-19 08:59:07 -0500

    Add docstrings with cross-refs to z.i.common.interfaces
    Also bring the ``classImplements`` lines closer to the interface
    declaration. This helped me be sure that nothing was missed and that
    all the repetitions of the names matched.

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ release = rqmt.version
 exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'obj'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
@@ -264,3 +264,4 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 autodoc_default_flags = ['members', 'show-inheritance']
 autoclass_content = 'both'
+autodoc_member_order = 'bysource'
