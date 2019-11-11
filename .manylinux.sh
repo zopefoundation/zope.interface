d88017c2c7af09dbd1a82dfbf9e21b1b71dae6ba
Marius Gedminas <marius@gedmin.as>
2019-11-11 14:53:33 +0200

    Always build manylinux wheels
    This PR makes it so we _always_ build manylinux wheels, but only upload
    them for git tags.
    
    Highlights:
    
    - Move regular Linux Python builds outside the build matrix, for
      conciseness.
    
    - Switch manylinux1 builds to manylinux2010 (they should still get the
      manylinux1 tag if they're compatible, but they'll be built with a more
      modern compiler toolchain).
    
    - Move twine upload from .manylinux.sh into .travis.yml's after_success,
      right next to the twine upload used for Mac OS builds.
    
    - This means we can't short-circuit the builds with 'exit 0' in
      before_install:, so overwrite the install: and script: sections too.
    
    - Move the 'docker pull' from .manylinux.sh into the install: section
      so it's not empty.
    
    - Provide twine credentials via environment variables instead of
      command-line/pypirc file.
    
    - Put related commands inside a single multiline if statement.
    
    - Use setup.py -q test (quiet build) instead of setup.py test -q (which
      does nothing at all AFAICT).
    
    This mirrors changes made to other zopefoundation packages tracked in
    https://github.com/zopefoundation/meta/issues/11.

--- .manylinux.sh
+++ .manylinux.sh
@@ -2,8 +2,4 @@
 
 set -e -x
 
-docker pull $DOCKER_IMAGE
-
-docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/.manylinux-install.sh
-
-pip install twine && twine upload -u zope.wheelbuilder -p $PYPIPASSWORD wheelhouse/*
+docker run --rm -v "$(pwd)":/io $DOCKER_IMAGE $PRE_CMD /io/.manylinux-install.sh
