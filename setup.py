08d724ec31de64c293183d957fd27278583ba21e
Marius Gedminas <marius@gedmin.as>
2020-03-10 09:52:27 +0200

    Back to development: 4.7.3

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ long_description = (
         )
 
 setup(name='zope.interface',
-      version='4.7.2',
+      version='4.7.3.dev0',
       url='https://github.com/zopefoundation/zope.interface',
       license='ZPL 2.1',
       description='Interfaces for Python',
