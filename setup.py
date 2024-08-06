6b443a6d7e5d378457f3724329fbd04f23a62703
Tres Seaver <tseaver@palladion.com>
2024-08-06 11:35:35 -0400

    chore: prep 7.0.1 release

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ long_description = (
 
 setup(
     name='zope.interface',
-    version='7.1.dev0',
+    version='7.0.1',
     url='https://github.com/zopefoundation/zope.interface',
     license='ZPL 2.1',
     description='Interfaces for Python',
