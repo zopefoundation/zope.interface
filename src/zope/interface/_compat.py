fda90bc4fcbdbaed9b77e4ffb96d31a7ca336d62
Michał Górny <mgorny@gentoo.org>
2024-10-19 16:34:00 +0200

    feat: Do not use C speedups on Python 3.13+ freethreading
    Do not enable C speedups by default if running Python 3.13+
    in freethreading mode (with GIL disabled).  Since the extension
    does not support this mode explicitly at the moment, loading it would
    cause CPython to reenable GIL with a warning and penalize the whole
    environment.
    
    Discussed in issue #330.

--- src/zope/interface/_compat.py
+++ src/zope/interface/_compat.py
@@ -74,14 +74,18 @@ def _should_attempt_c_optimizations():
     """
     Return a true value if we should attempt to use the C optimizations.
 
-    This takes into account whether we're on PyPy and the value of the
-    ``PURE_PYTHON`` environment variable, as defined in `_use_c_impl`.
+    This takes into account whether we're on PyPy, whether you're using
+    a freethreading version of Python 3.13+ (where importing
+    the speedups would enable GIL) and the value of the ``PURE_PYTHON``
+    environment variable, as defined in `_use_c_impl`.
     """
     is_pypy = hasattr(sys, 'pypy_version_info')
 
     if _c_optimizations_required():
         return True
     if is_pypy:
+        return False
+    if sys.version_info >= (3, 13) and not sys._is_gil_enabled():
         return False
     return not _c_optimizations_ignored()
 
